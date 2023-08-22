4d0e28db3bd4400ee71e47ef0e681726af90a38c
khouloud HADDAD AMAMOU <khouloud.haddad@gmail.com>
2023-08-22 14:55:57 +0000

    CREATE COMPONENT

--- app/Http/Livewire/TeamMember/Create.php
+++ app/Http/Livewire/TeamMember/Create.php
@@ -3,9 +3,22 @@
 namespace App\Http\Livewire\TeamMember;
 
 use Livewire\Component;
+use Livewire\Attributes\Rule;
+use Livewire\Component;
 
 class Create extends Component
 {
+    #[Rule(['required', 'string', 'min:3'])]
+    public string $name;
+ 
+    #[Rule(['required', 'string', 'min:3'])]
+    public string $timezone;
+ 
+    public function createMember()
+    {
+        TeamMember::create($this->validate());
+        $this->redirectRoute('index');
+    }
     public function render()
     {
         return view('livewire.team-member.create');
